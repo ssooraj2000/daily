9b914cfeb5d919c71f19ea20166b3bf97b7b0c89
ssooraj2000 <51398824+ssooraj2000@users.noreply.github.com>
2020-05-09 23:18:00 +0530

    Add files via upload

--- codechefday1.cpp
+++ codechefday1.cpp
@@ -1,22 +1,77 @@
+//You have a set of tiles, where each tile has one letter tiles[i] printed on it.  Return the number of possible non-empty sequences of letters you can make.
+
 #include<iostream>
 #include<cstdlib>
-
+#include<cmath>
+#include<string>
 using namespace std;
 
-string toLowerCase(string str){
-
-	int i;
+int fact(int num){
+	if(num==0){
+		return 1;
+	}
+	return num*fact(num-1);
+}
+int findComb(string str){
+	int arr[52]={0};
+	int i,f=1,combi;
 	for(i=0;i<str.length();i++){
-		if(str[i]>=65 && str[i]<= 90){
-			str[i]=str[i]+32;
+		if(str[i]>=65 && str[i]<=90){
+			arr[str[i]-39]++;
+		}
+		else{
+			arr[str[i]-97]++;
 		}
 	}
-	return str;
+	combi=fact(str.length());
+	for(i=0;i<52;i++){
+		if(arr[i]>1){
+			f=f*fact(arr[i]);
+		}
+	}
+	combi=combi/f;
+	return combi;
+}
+int solve(string tiles){
+	
+	int size=pow(2, tiles.length());
+	int count;
+	int i, j;
+	string arr[size];
+ 
+	for(i = 1,count=0;i<pow(2, tiles.length());i++,count++){
+		for(j=0;j<tiles.length();j++){
+			if(i & 1<<j){
+			arr[count]=arr[count]+tiles[j];
+			}	
+		}
+	}
+	for(i=0;i<pow(2, tiles.length())-1;i++){
+		for(j=i+1;j<pow(2, tiles.length());j++){
+			if(arr[i].compare(arr[j])==0){
+				arr[j]=" ";
+			}
+		}
+	}
+
+	// for(i=0;i<pow(2, tiles.length())-1;i++){
+	// 	cout<<arr[i]<<"\n";
+	// }
+	int seq=0;
+
+	for(i=0;i<pow(2, tiles.length())-1;i++){
+		if(arr[i]!=" "){
+
+			seq=seq+findComb(arr[i]);
+		}
+	}
+	
+return seq;
 }
 
 int main(){
-	string str;
-	cin >> str;
-	cout << toLowerCase(str)<<"\n";
-	
+string tiles;
+cin>>tiles;
+
+cout << solve(tiles)<<"\n";	
 }
