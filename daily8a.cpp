b8ec712b0646fcccbc59f92995951d2fdc225345
ssooraj2000 <51398824+ssooraj2000@users.noreply.github.com>
2020-05-16 11:04:09 +0530

    Update daily8a.cpp

--- daily8a.cpp
+++ daily8a.cpp
@@ -1,4 +1,4 @@
-//
+//Given an array A[] of N positive integers which can contain integers from 1 to N where elements can be repeated or can be absent from the array. Your task is to count frequency of all elements from 1 to N.
 #include <bits/stdc++.h> 
 using namespace std; 
 
