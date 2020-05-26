ca2212214f7145ea7bb17ebf21426249f71e5b53
ssooraj2000 <51398824+ssooraj2000@users.noreply.github.com>
2020-05-26 19:25:32 +0530

    Sort lowercase and uppercase of string separately in their range
    Given a string S consisting of uppercase and lowercase characters. The task is to sort uppercase and lowercase letters separately such that if the ith place in the original string had an Uppercase character then it should not have a lowercase character after being sorted and vice versa.

--- daily18d.cpp
+++ daily18d.cpp
@@ -1,44 +1,39 @@
-//Write a program to find transpose of a square matrix mat[][] of size N*N. Transpose of a matrix is obtained by changing rows to columns and columns to rows.
-#include <bits/stdc++.h> 
+Given a string S consisting of uppercase and lowercase characters. The task is to sort uppercase and lowercase letters separately such that if the ith place in the original string had an Uppercase character then it should not have a lowercase character after being sorted and vice versa.
+ #include <bits/stdc++.h> 
 using namespace std; 
 
-void solve(int** arr,int n){
-    int i,j,temp;
+void solve(string s,int n){
+    int i,csmall=0, ccaps=0;
+    string small, caps;
     for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(arr[i][j]>=0){
-                temp=arr[i][j];
-                arr[i][j]=-1*arr[j][i];
-                arr[j][i]=-1*temp;
-            }
+        if(s[i]>=97 && s[i]<=122){
+            small.push_back(s[i]);
+            csmall++;
+        }
+        else{
+            caps.push_back(s[i]);
+            ccaps++;
         }
     }
+    sort(small.begin(), small.end());
+    sort(caps.begin(), caps.end());
+    int sm=0, cp=0;
     for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cout<<abs(arr[i][j])<<" ";
+        if(s[i]>=97 && s[i]<=122){
+            s[i]=small[sm++];
         }
-        cout<<"\n";
+        else{
+            s[i]=caps[cp++];
+        }
     }
+    cout<<s;
 }
 
 int main() 
 { 
-    int n;
-    cout<<"n:";
-    cin>>n;
-    
-    int i;
-    int** arr=(int**)malloc(sizeof(int*)*n);
-    for(i=0;i<n;i++){
-        arr[i]=(int*)malloc(sizeof(int)*n);
-    }
-    int j;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cin>>arr[i][j];
-        }
-    }
-    solve(arr, n);
+    string s;
+    cin>>s;
+    solve(s,s.length());
 	return 0; 
 } 
 
