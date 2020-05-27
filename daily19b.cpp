24d0818e854c7df9ed4e528924c1b4d210b909b5
ssooraj2000 <51398824+ssooraj2000@users.noreply.github.com>
2020-05-28 00:27:07 +0530

    Swap triangles on either side of diagonal
    Given a 2-D array of order NxN, swap the values of the triangle above the diagonal with the values of the triangle below it like a mirror image swap. Print the 2-D array obtained in matrix layout. Let the elements of the 2-D array be denoted by A[ i ][ j ], where i, j vary from 0 to N-1.

--- daily19b.cpp
+++ daily19b.cpp
@@ -1,38 +1,45 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+//Given a 2-D array of order NxN, swap the values of the triangle above the diagonal with the values of the triangle below it like a mirror image swap. Print the 2-D array obtained in matrix layout. Let the elements of the 2-D array be denoted by A[ i ][ j ], where i, j vary from 0 to N-1.
+ #include <bits/stdc++.h> 
+ using namespace std; 
 
-void solve(int** arr,int n){
-    int i,j;
-    int* coil1=(int*)malloc(sizeof(int)*((n*n)/2));
-    int* coil2=(int*)malloc(sizeof(int)*((n*n)/2));
+ void solve(int** arr,int n){
+     int i=0,j=1,temp;
+     while(i<n && j<n){
+         temp=arr[i][j];
+         arr[i][j]=arr[j][i];
+         arr[j][i]=temp;
+         if(j+1<n){
+             j++;
+         }
+         else{
+             i++;
+         }
+     }
+     for(i=0;i<n;i++){
+         for(j=0;j<n;j++){
+             cout<<arr[i][j]<<" ";
+         }
+         cout<<"\n";
+     }
+ }
 
-  
-    for(i=0;i<n*n/2;i++){
-        cout<<coil1[i]<<" ";
-    }
-    cout<<"\n"; 
+ int main() 
+ { 
+     int n;
+     cout<<"n:";
+     cin>>n;
 
-}
-
-int main() 
-{ 
-    int n;
-    cout<<"n:";
-    cin>>n;
-
-    n=n*4;
-    int** arr=(int**)malloc(sizeof(int*)*n);
-    int i;
-    for(i=0;i<n;i++){
-        arr[i]=(int*)malloc(sizeof(int)*n);
-    }
-    int j;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cin>>arr[i][j];
-        }
-    }
-    solve(arr,n);
-  return 0;
-} 
-
+     int** arr=(int**)malloc(sizeof(int*)*n);
+     int i;
+     for(i=0;i<n;i++){
+         arr[i]=(int*)malloc(sizeof(int)*n);
+     }
+     int j;
+     for(i=0;i<n;i++){
+         for(j=0;j<n;j++){
+             cin>>arr[i][j];
+         }
+     }
+     solve(arr,n);
+   return 0;
+ } 
