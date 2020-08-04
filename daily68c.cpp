c167c1c137d7426f0d31fcdacee8bac1a61a1345
Sooraj Sowmya Narayanan <ssooraj2000@yahoo.com>
2020-08-04 14:46:34 +0530

    Left Rotate Array

---
+++ daily68c.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> solve(vector<int> arr, int d){
+    int i, j, temp;
+    for(i=0;i<d;i++){
+        temp=arr[0];
+        for(j=0;j<arr.size()-1;j++){
+            arr[j]=arr[j+1];
+        }
+        arr[j]=temp;
+    }
+    return arr;
+}
+int main(){
+    vector<int> arr;
+    int n, d, i;
+    cin>>n;
+    cin>>d;
+    for(i=0;i<n;i++){
+        int data;
+        cin>>data;
+        arr.push_back(data);
+    }
+    vector<int> result=solve(arr, d);
+
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    cout<<"\n";
+    
+    return 0;
+}
